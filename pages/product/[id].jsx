4d9ec8e912d2b9be621427282e250771e5e6e2a6
joaoAugustoParaty <joaodrow@gmail.com>
2022-03-05 11:33:33 -0300

    Toolkit

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,12 +2,15 @@ import styles from "../../styles/Product.module.css";
 import Image from "next/image";
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import {addProduct} from "../../redux/cartSlice";
 
 const Product = ({pizza}) => {
   const [price, setPrice] = useState(pizza.prices[0]);
   const [size, setSize] = useState(0);
   const [quantity, setQuantity] = useState(1);
   const [extras, setExtras] = useState([]);
+  const dispatch = useDispatch();
 
   const changePrice = (number) => {
     setPrice(price+number)
@@ -30,6 +33,9 @@ const Product = ({pizza}) => {
     }
   }
   
+const handleClick = () => {
+  dispatch(addProduct({...pizza,extras,price,quantity}))
+};
 
   return (
     <div className={styles.container}>
@@ -75,7 +81,7 @@ const Product = ({pizza}) => {
         </div>
         <div className={styles.add}>
             <input onChange={(e)=>setQuantity(e.target.value)} type="number" defaultValue={1} className={styles.quantity}/>
-            <button className={styles.button}>Add to Cart</button>
+            <button className={styles.button} onClick={handleClick}>Add to Cart</button>
         </div>
       </div>
     </div>
